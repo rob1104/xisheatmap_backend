454101c2d6c23072e9f80410dd1c7895e79dd69c
Robbie Escamilla (Computadora Xis) <rescamillamtz@gmail.com>
2026-04-13 19:47:40 -0500

    Nuevo menu

--- resources/js/Layouts/AdminLayout.vue
+++ resources/js/Layouts/AdminLayout.vue
@@ -37,6 +37,13 @@
                     <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7"></path></svg>
                     Mapa de Calor
                 </Link>
+
+                <Link :href="route('logs.index')"
+                      :class="{'bg-indigo-800 text-white': route().current('logs.index'), 'text-indigo-100 hover:bg-indigo-800': !route().current('logs.index')}"
+                      class="flex items-center px-3 py-2.5 rounded-lg transition-colors group">
+                    <svg class="w-5 h-5 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+                    Logs de Auditoria
+                </Link>
             </nav>
 
             <div class="p-4 border-t border-indigo-800">

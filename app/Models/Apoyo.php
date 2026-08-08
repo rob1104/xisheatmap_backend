4f7e77dd6125fd899cddbee1219aff81d30ec8b0
Robbie Escamilla (Computadora Xis) <rescamillamtz@gmail.com>
2026-08-07 19:36:13 -0500

    Logs Apoyo

--- app/Models/Apoyo.php
+++ app/Models/Apoyo.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Apoyo extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
 
     protected $fillable = [
         'nombre',
@@ -19,4 +21,12 @@ class Apoyo extends Model
         'apoyo',
         'estatus_de_apoyo',
     ];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logAll()
+            ->logOnlyDirty()
+            ->setDescriptionForEvent(fn(string $eventName) => "Apoyo {$eventName}");
+    }
 }
